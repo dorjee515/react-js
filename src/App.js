b6216de29e8b9027b94931b8db8eba8c375dc025
pratap dorjee <pratapdorjee515@gmail.com>
2020-12-22 13:27:35 +0530

    react commit component 2 with card

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 import {Navbar,NavbarBrand} from 'reactstrap';
 import Menu from './components/menucomponent';
+import {DISHES} from './shared/dishes';
+import React,{ Component } from 'react';
 
-import { Component } from 'react';
-function App() {
-  return (
+class App extends Component{  
+
+  constructor(props){
+   super(props);
+   this.state={
+     dishes:DISHES
+    };
+   }
+  render(){
+  return ( 
     <div>
       <Navbar dark color="primary">
       <div classname="container">
         <NavbarBrand href="/">food is good</NavbarBrand>
       </div>
       </Navbar>
-      <Menu/>
+      <Menu dishes={this.state.dishes}/>
     </div>
   );
+  }
 }
 
 export default App;
